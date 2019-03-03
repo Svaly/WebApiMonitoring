f808c80b60a7fd727ad74265abcee2fc76cff913
Rafal <rafal.grzesik94@gmail.com>
2019-03-03 09:25:50 +0100

    InitialCommit

---
+++ WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using Swashbuckle.Application;
+
+namespace WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.EnableSwagger(c => c.SingleApiVersion("v1", "WebApi")).EnableSwaggerUi();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("api/values")]
+    public class ValuesController : ApiController
+    {
+        [Route()]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [Route()]
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        [Route("{id}")]
+        [HttpPost]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        [Route("{id}")]
+        [HttpDelete]
+        public void Delete(int id)
+        {
+        }
+    }
+}
